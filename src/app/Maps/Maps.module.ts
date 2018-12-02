be494ac52a1711cd2dedcf8b74ded47fa853ae1f
Aleeta Yusoh <584235031@parichat.skru.ac.th>
2018-12-02 17:14:15 +0700

    rnnn

--- src/app/Maps/Maps.module.ts
+++ src/app/Maps/Maps.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import{ MapsPage }from './Maps.page';
-
+import { AgmCoreModule } from '@agm/core';
 
 
 @NgModule({
@@ -12,8 +12,15 @@ import{ MapsPage }from './Maps.page';
     IonicModule,
     CommonModule,
     FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAFgM81Qz-SwfTzUsr4F51AgDj0HdN88CQ' //Google API key for maps
+    }),
     RouterModule.forChild([{ path: '', component: MapsPage }])
+    
   ],
   declarations: [MapsPage]
 })
-export class MapsPageModule {}
+export class MapsPageModule {
+  lat: number = 26.765844;
+  lng: number = 83.364944;
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,19 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { from } from 'rxjs';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports:
+    [
+      BrowserModule,
+      IonicModule.forRoot(),
+      AppRoutingModule,
+     
+    ],
+
+
   providers: [
     StatusBar,
     SplashScreen,
@@ -22,4 +29,4 @@ import { from } from 'rxjs';
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
