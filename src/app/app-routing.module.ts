05a3bfabb0866b1bd9eb7fe8de3a2b914ab4ca0f
Aleeta Yusoh <584235031@parichat.skru.ac.th>
2018-12-02 12:03:59 +0700

    ล่าสุด

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
   { path: 'list', loadChildren: './list/list.module#ListPageModule' },
-  { path: 'seach', loadChildren: './seach/seach.module#SeachPageModule' }
+ 
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
